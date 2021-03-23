08c4d05e2514fbcef7428a561baff797006145a2
Gustavo Silva <gstvds@icloud.com>
2021-03-23 09:27:59 -0300

    feat: Adds addProduct functionality and retrieve cart from localStorage

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -2,6 +2,7 @@ import { createContext, ReactNode, useContext, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
 import { Product, Stock } from '../types';
+import { CART_STORAGE_KEY } from '../util/constants';
 
 interface CartProviderProps {
   children: ReactNode;
@@ -19,24 +20,47 @@ interface CartContextData {
   updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
 }
 
+type ProductWithoutAmount = Omit<Product, 'amount'>;
+
 const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem(CART_STORAGE_KEY);
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const getStock = await api.get<Stock>(`/stock/${productId}`);
+      const { amount } = getStock.data;
+      if (amount <= 1) throw new Error('Out of stock');
+
+      let checkItemExists = cart.find((item) => item.id === productId);
+      let updateCart: Product[] = [];
+
+      if (!checkItemExists) {
+        const { data: productData } = await api.get<ProductWithoutAmount>(`/products/${productId}`);
+        checkItemExists = { ...productData, amount: 1 };
+
+        updateCart = [...cart, checkItemExists];
+      } else {
+        updateCart = cart.map((item) => {
+          if (item.id === productId) return { ...item, amount: item.amount + 1 }
+          return item;
+        });
+      }
+
+      setCart(updateCart);
+      await api.patch(`/stock/${productId}`, { amount: amount - 1 });
+      localStorage.setItem(CART_STORAGE_KEY, JSON.stringify(cart));
     } catch {
-      // TODO
+      toast.error('Quantidade solicitada fora de estoque');
     }
   };
 
